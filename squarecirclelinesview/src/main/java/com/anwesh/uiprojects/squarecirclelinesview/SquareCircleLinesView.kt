88116ec7307101f4059aa4582f21517c1164dabd
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 11:36:57 +0530

    adding Int and Float extension methods

--- squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
+++ squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
@@ -23,3 +23,13 @@ val circleColor : Int = Color.parseColor("#01579B")
 val lineColor : Int = Color.parseColor("#00C853")
 val sqColor : Int = Color.parseColor("#FFAB00")
 val backColor : Int = Color.parseColor("#212121")
+
+fun Int.inverse() : Float = 1f / this
+fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+fun Float.mirrorValue(a : Int, b : Int) : Float {
+    val k : Float = scaleFactor()
+    return (1 - k) * a.inverse() + k * b.inverse()
+}
+fun Float.updateValue(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
