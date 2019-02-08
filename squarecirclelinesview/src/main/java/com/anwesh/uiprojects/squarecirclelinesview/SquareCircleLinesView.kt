a0e5e086b322a8deab9228af49d92ca84fe1fc0e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 12:21:42 +0530

    adding SquareCircleLines class

--- squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
+++ squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
@@ -194,7 +194,31 @@ class SquareCircleLinesView(ctx : Context) : View(ctx) {
                 return curr
             }
             cb()
-            return this 
+            return this
+        }
+    }
+
+    data class SquareCircleLines(var i : Int) {
+
+        private val root : SCLNode = SCLNode(0)
+        private var curr : SCLNode = root
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            root.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
         }
     }
 }
