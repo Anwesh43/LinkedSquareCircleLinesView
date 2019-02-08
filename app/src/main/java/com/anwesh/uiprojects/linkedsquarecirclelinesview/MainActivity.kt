6f1a72614e86aea2188b7b0cf12707b2d4ee3b6f
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 12:42:34 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarecirclelinesview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarecirclelinesview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedsquarecirclelinesview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.squarecirclelinesview.SquareCircleLinesView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         SquareCircleLinesView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
