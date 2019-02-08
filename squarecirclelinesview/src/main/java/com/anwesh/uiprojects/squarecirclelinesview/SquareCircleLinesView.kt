cff8d7dba195db6723ad84dadcc144c462705e8b
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 11:33:38 +0530

    adding libraries and defining constants

--- squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
+++ squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
@@ -4,3 +4,22 @@ package com.anwesh.uiprojects.squarecirclelinesview
  * Created by anweshmishra on 08/02/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.content.Context
+import android.app.Activity
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+
+val nodes : Int = 5
+val lines : Int = 3
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.9f
+val strokeFactor : Int = 90
+val circleColor : Int = Color.parseColor("#01579B")
+val lineColor : Int = Color.parseColor("#00C853")
+val sqColor : Int = Color.parseColor("#FFAB00")
+val backColor : Int = Color.parseColor("#212121")
