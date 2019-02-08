35794d18472b297ccbfab3630f61aec849400f1e
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 11:58:17 +0530

    adding drawSCLNode canvas extension method

--- squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
+++ squarecirclelinesview/src/main/java/com/anwesh/uiprojects/squarecirclelinesview/SquareCircleLinesView.kt
@@ -32,4 +32,58 @@ fun Float.mirrorValue(a : Int, b : Int) : Float {
     val k : Float = scaleFactor()
     return (1 - k) * a.inverse() + k * b.inverse()
 }
-fun Float.updateValue(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap 
+fun Float.updateValue(dir : Int, a : Int, b : Int) : Float = mirrorValue(a, b) * dir * scGap
+
+fun Canvas.drawSquare(size : Float, paint : Paint) {
+    paint.color = sqColor
+    drawRect(RectF(-size, -size, size, size), paint)
+}
+
+fun Canvas.drawLine(x : Float, size : Float, paint : Paint) {
+    paint.color = lineColor
+    paint.strokeCap = Paint.Cap.ROUND
+    save()
+    translate(x, 0f)
+    drawLine(0f, 0f, size, 0f, paint)
+    restore()
+}
+
+fun Canvas.drawCircle(r : Float, paint : Paint) {
+    paint.color = circleColor
+    drawCircle(0f, 0f, r, paint)
+}
+
+fun Canvas.drawBiLine(x : Float, y : Float, size : Float, paint : Paint) {
+    for (j in 0..1) {
+        save()
+        translate(0f, y)
+        scale(1f - 2 * j, 1f)
+        drawLine(x, size, paint)
+        restore()
+    }
+}
+
+fun Canvas.drawSCLNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val r : Float = size / 3
+    val lx : Float = size / 2
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val sc21 : Float = sc2.divideScale(0, 2)
+    val sc22 : Float = sc2.divideScale(1, 2)
+    val yGap : Float = (2 * size) / lines
+    save()
+    translate(w/2, gap * (i + 1))
+    rotate(90f * sc22)
+    drawSquare(size * sc21, paint)
+    drawCircle(r * sc1, paint)
+    for (j in 0..(lines - 1)) {
+        val sc : Float = sc1.divideScale(j, lines)
+        drawBiLine(lx, yGap * j, r * sc, paint)
+    }
+    restore()
+}
